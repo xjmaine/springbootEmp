8f72def70bfb9feeed407f131b1d481c11e4ce5d
Freddie <xjfreddie@gmail.com>
2021-05-31 17:09:46 +0000

    Complete project initial

---
+++ src/main/java/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import domain.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
